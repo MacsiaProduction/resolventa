f93be6c8ac1e5a2d835a026d384c13bec7645b5a
stleubaev <tleubaev.stepan@gmail.com>
2024-05-22 21:27:00 +0700

    Реализовать передачу pdf файла в ответ на POST запрос + README

--- src/main/java/com/resolventa/solvers/NaturalSequentSolver.java
+++ src/main/java/com/resolventa/solvers/NaturalSequentSolver.java
@@ -1,8 +1,10 @@
 package com.resolventa.solvers;
 
-import com.resolventa.prosol.Problem;
 import com.resolventa.execruns.ExecFileRunner;
+import com.resolventa.execruns.FileToBase64Converter;
 import com.resolventa.execruns.NotZeroExitCodeException;
+import com.resolventa.execruns.SaveStringToFile;
+import com.resolventa.prosol.Problem;
 import com.resolventa.prosol.Solution;
 
 import java.io.IOException;
@@ -15,7 +17,7 @@ public class NaturalSequentSolver implements Solver{
     public Solution solve(Problem problem) {
         Solution res;
 
-        List<String> command = new ArrayList<>();
+        final List<String> command = new ArrayList<>();
         command.add("./raw/wang");
         command.add("-s");
         command.add(problem.problem_content);
@@ -23,13 +25,34 @@ public class NaturalSequentSolver implements Solver{
         command.add("LaTeX");
 
         ExecFileRunner runner = new ExecFileRunner(command);
+        String latex_code, pdf_base64;
 
         try {
-            res = new Solution(problem.problem_type, runner.Run());
+            latex_code = wrap_latex(runner.Run());
+            SaveStringToFile.save("./raw/res.tex", latex_code);
+
+            command.clear();
+            command.add("bash");
+            command.add("-c");
+            command.add("cd raw/ && pdflatex res.tex");
+            runner.Run();
+
+            pdf_base64 = FileToBase64Converter.convert("./raw/res.pdf");
         } catch (IOException | InterruptedException | NotZeroExitCodeException e) {
             res = new Solution(ERROR_TYPE, e.getMessage());
+            return res;
         }
 
+        res = new Solution(problem.problem_type, pdf_base64);
         return res;
+    }
+
+    private String wrap_latex(String latex){
+        return "\\documentclass{article}\n" +
+                "\\usepackage{bussproofs}\n" +
+                "\\usepackage{amsmath}\n" +
+                "\\begin{document}\n" +
+                latex +
+                "\\end{document}";
     }
 }
